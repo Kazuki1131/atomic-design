127151c711aeedbc89cedb63ecdfb6474ecd72a8
Kazuki1131 <onetsubyo@gmail.com>
2023-05-05 20:53:31 +0900

    Add Testing Library Config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-/** @type {import('next').NextConfig} */
+/**  @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	compiler: (() => {
+		if (process.env.NODE_ENV === 'production') {
+			return {
+				// 本番環境ではReact Testing Libraryで使用するdata-testid属性を削除
+				reactRemoveProperties: { properties: ['^data-testid$'] },
+			};
+		}
+	})(),
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
